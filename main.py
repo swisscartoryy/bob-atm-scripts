454f2eb29bd4ca3c5783d312d28b8cba9c4ba39d
swisscartoryy <swisscartoryy@gmail.com>
2026-01-31 20:40:12 +0100

    add branches, kiosks

--- main.py
+++ main.py
@@ -1,6 +1,55 @@
-def main():
-    print("Hello from scripts!")
+import os
+import json
 
+from bs4 import BeautifulSoup
+from tabulate import tabulate
 
-if __name__ == "__main__":
-    main()
+html_paths = [
+    "assets/bcp_atms.html",
+    "assets/bcp_kiosks.html",
+    "assets/bcp_branches.html",
+]
+
+departmentids = [
+    "fondoOficinaLaPaz",
+    "fondoOficinaSantaCruz",
+    "fondoOficinaOruro",
+    "fondoOficinaCochabamba",
+    "fondoOficinaPotosi",
+    "fondoOficinaChuquisaca",
+    "fondoOficinaTarija",
+    "fondoOficinaTrinidad",
+]
+
+for html_path in html_paths:
+    with open(html_path, "r", encoding="utf-8") as file:
+        html = file.read()
+
+    soup = BeautifulSoup(html, "html.parser")
+    filename = f"{html_path[len("assets/bcp_") : -len(".html")]}.json"
+
+    for departmentid in departmentids:
+        element = soup.find(id=departmentid)
+        dirname = f"assets/{departmentid[len("fondooficina") :]}".lower()
+
+        os.makedirs(dirname, exist_ok=True)
+
+        if element:
+            rsrows = element.select("table > tr")
+            rsheaders = element.select("table > thead > tr > th")
+
+            rows: list[list[str]] = []
+            headers = [h.get_text(strip=True) for h in rsheaders]
+
+            for rsrow in rsrows:
+                rsprops = rsrow.select("td")
+                props = [rsprop.get_text(strip=True) for rsprop in rsprops]
+                rows.append(props)
+
+            drows = [dict(zip(headers, row)) for row in rows]
+
+            json_path = f"{dirname}/{filename}"
+            json_data = json.dumps(drows, ensure_ascii=False, indent=4)
+
+            with open(json_path, "w", encoding="utf-8") as file:
+                file.write(json_data)
