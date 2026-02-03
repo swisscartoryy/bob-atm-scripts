623758862acb457f8da366688136d07922608340
swisscartoryy <swisscartoryy@gmail.com>
2026-02-03 15:27:11 +0100

    add bnb json > csv

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import csv
 import json
 
-from src.dtos.bmsc import BmscATM, BmscBranch
 from src.dtos.bisa import BisaBranchATM
+from src.dtos.bmsc import BmscATM, BmscBranch
+from src.dtos.bnb import BnbDepartamento, BnbBranchATM
 
-filename = "assets/bmsc/branches.json"
+filename = "assets/bnb/branchatms.json"
 
 # opening json
 with open(
@@ -13,7 +14,24 @@ with open(
     encoding="utf-8",
 ) as jsonfile:
     jbranchatms = json.load(jsonfile)
-    branchatms = [BmscBranch(**jbranchatm) for jbranchatm in jbranchatms]
+    departamentos = [
+        BnbDepartamento.model_validate(jbranchatm) for jbranchatm in jbranchatms
+    ]
+
+    branchatms = [
+        branchatm
+        for departamento in departamentos
+        for branchatm in departamento.detalles
+    ]
+
+# filters
+# rows = []
+# filters = []
+# for branchatm in branchatms:
+#     if branchatm.subtipo not in filters:
+#         filters.append(branchatm.subtipo)
+#         rows.append(branchatm.model_dump())
+# print(len(filters), filters)
 
 # writting csv
 rows = [branchatm.model_dump() for branchatm in branchatms]
---
+++ src/dtos/bnb/__init__.py
@@ -0,0 +1,7 @@
+from .enums import (
+    BnbTipoEntidadId,
+    BnbDepartamentoId,
+    BnbSubtipoEntidadId,
+)
+
+from .bnb_branchatm import BnbBranchATM, BnbDepartamento
