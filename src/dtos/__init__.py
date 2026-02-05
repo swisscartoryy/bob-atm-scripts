dbe94f682eef9999ba6c6fc200c04672556e1ee6
swisscartoryy <swisscartoryy@gmail.com>
2026-02-05 02:19:21 +0100

    update init.py

--- src/dtos/__init__.py
+++ src/dtos/__init__.py
@@ -1,6 +1,7 @@
 from .bg import gana_branchatm
 from .bnb import enums, bnb_branchatm
 from .bisa import enums, bisa_branchatm
+from .bmsc import const, bmsc_branchatm
 from .baneco import enums, baneco_branchatm
 from .bancosol import const, bancosol_branchatm
 from .bancounion import const, bancounion_branchatm
